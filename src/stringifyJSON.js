d00ac066aa6f1c930cc451cee9d27f9659d7b91d
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-18 20:55:05 -0700

    implement stringifyJSON in es6

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,24 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
-var stringifyJSON = function(obj) {
-  // your code goes here
+const stringifyJSON = obj => {
+  if (typeof obj === 'function' ||
+      typeof obj === 'undefined') {
+    return null;
+  }
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+  if (Array.isArray(obj)) {
+    let result = obj.map(item => stringifyJSON(item));
+    return '[' + result.join(',') + ']';
+  }
+  if (obj === Object(obj)) {
+    let result = [];
+    for (let key of Object.keys(obj)) {
+      let val = stringifyJSON(obj[key]);
+      if (val !== null) {
+        result.push('\"' + key + '\":' + val);
+      }
+    }
+    return '{' + result.join(',') + '}';
+  }
+  return '' + obj;
 };

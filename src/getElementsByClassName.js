7e03342711f20c70c8e48cf0e6d00f13c7f7c148
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-18 21:03:33 -0700

    implement getElementsByClassName in es6

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,15 +1,14 @@
-var getElementsByClassName = function(className, node) {
-  var result = [];
-  node = node || document.body;
+const getElementsByClassName = (className, node = document.body) => {
+  let result = [];
   if (node.classList) { // text nodes have undefined classList
     if (node.classList.contains(className)) {
       result.push(node);
     }
   }
-  node.childNodes.forEach(function(childNode) { 
+  for (let childNode of node.childNodes) {
     // childNodes is always an array, even if it's empty for text nodes
     result = result.concat(
       getElementsByClassName(className, childNode));
-  });
+  }
   return result;
 };

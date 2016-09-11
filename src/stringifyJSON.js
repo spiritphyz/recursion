1ee1c8e1c799bbe88aaf7ef0920cd6a1ee55233f
spiritphyz <tonyle@ocf.berkeley.edu>
2016-09-10 18:58:18 -0700

    use 'const' instead of 'let'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,11 +7,11 @@ const stringifyJSON = obj => {
     return '\"' + obj + '\"';
   }
   if (Array.isArray(obj)) {
-    let result = obj.map(item => stringifyJSON(item));
+    const result = obj.map(item => stringifyJSON(item));
     return '[' + result.join(',') + ']';
   }
   if (obj === Object(obj)) {
-    let result = [];
+    const result = [];
     for (let key of Object.keys(obj)) {
       let val = stringifyJSON(obj[key]);
       if (val !== null) {
